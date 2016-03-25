de5f60cac68934e92b848b650f7d1a1c38fb323a
Xiaofei Long <dreloong@gmail.com>
2016-03-25 17:13:01 -0500

    Initial commit

---
+++ OneChain/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  OneChain
+//
+//  Created by Xiaofei Long on 3/25/16.
+//  Copyright © 2016 3archers. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(
+        application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?
+    ) -> Bool {
+        return true
+    }
+}
